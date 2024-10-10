300bee15507efd86bb6cd1582367e26f097ac0db
Bkstone05 <bkstone42@tntech.edu>
2024-10-10 12:37:25 -0500

    Start of Animal Class

--- Zoo.cpp
+++ Zoo.cpp
@@ -1,1 +1,9 @@
+/****************************
+ * Name: Zoo.cpp
+ * Purpose: Stores all functions for class Animal
+ * Author:
+ */
 
+#include "Zoo.h"
+
+void Animal::setName(string name);
--- Zoo.h
+++ Zoo.h
@@ -1,1 +1,60 @@
+/*********************
+ * Name: Zoo.h 
+ * Purpose: 
+ * Author:
+ */
 
+#include <string>
+#include <iostream>
+#include <string>
+using namespace std; 
+
+#ifndef Zoo_H
+#define Zoo_H
+
+class Animal
+{
+    private:
+    //variables
+    string name, food;
+    int age;
+    float weight;
+
+    public:
+    //Constructors
+    Animal()
+    {
+        this->name = " ";
+        this->food = " ";
+        this->age = 0;
+        this->weight = 0;
+    }
+
+    Animal(string name, string food, int age, float weight)
+     {
+        this->name = name;
+        this->food = food;
+        this->age = age;
+        this->weight = weight;
+     }
+
+     //setters
+     void setName(string name);
+     void setFood(string food);
+     void setAge(int age);
+     void setWeight(int weight);
+
+     //getters
+     string getName() const;
+     string getFood() const;
+     int getAge() const;
+     float getWeight() const; 
+
+     ~Animal()
+     {
+        cout << "This animal has passed away";
+     }
+};
+
+
+#endif
