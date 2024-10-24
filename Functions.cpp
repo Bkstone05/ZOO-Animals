18020132a537a83f98aa911e1ac01382907ba9d2
Bkstone05 <bkstone42@tntech.edu>
2024-10-23 20:11:45 -0500

    Commiting Functions.cpp to be tracked

---
+++ Functions.cpp
@@ -0,0 +1,30 @@
+/*
+    FILE NAME: Funtions.cpp
+
+    PURPOSE: Conatains all of the funcntions that are called within the Driver.cpp files       
+*/
+
+void getInitialAnimals(/*Need something here Probably pass by reference*/)
+{
+
+}
+
+void chosePrint()
+{
+
+}
+
+void addNode()
+{
+
+}
+
+void printAnimal()
+{
+
+}
+
+void printList()
+{
+    
+}
